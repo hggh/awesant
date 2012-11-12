e8f018a7938896ee0e9e52385f7a6517644ad869
Jonny Schulz <support@bloonix.de>
2012-11-12 16:26:17 +0100

    re-create the md files

--- perl/lib/Awesant/Input/File.pm
+++ perl/lib/Awesant/Input/File.pm
@@ -32,7 +32,7 @@ Experimental feature.
 If the option save_position is set to true then the last position
 with the inode of the log file is saved to a file. If Awesant is down
 then it can resume its work where it was stopped. This is useful if you
-want to lose less data as possible of your log files.
+want to lose as less data as possible of your log files.
 
 Please note that this feature is experimental and does not keep log file
 rotations.
